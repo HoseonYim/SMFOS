85d03763352c4fe2da6769cc01cc06201f6c8cb9
dacoev <justin.yim14@gmail.com>
2021-07-12 23:50:12 +0900

    info message

--- src/main/java/SMFOS/Main.java
+++ src/main/java/SMFOS/Main.java
@@ -2,16 +2,21 @@ package SMFOS;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public final class Main extends JavaPlugin {
+
+    Logger logger = getServer().getLogger();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        logger.info("SMFOS is activated");
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        logger.info("SMFOS is unactivated");
+
     }
 }
